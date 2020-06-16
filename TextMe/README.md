a15660c546b826edf3f9f9b042ddee80c5f7103e
Martin <Martin@MacBook-Air-Martin.local>
2020-06-16 21:24:39 +0200

    adding textme partial solution

---
+++ TextMe/README.md
@@ -0,0 +1,2 @@
+This is a folder dedicated to this challenge: 
+https://ironscripter.us/text-me-a-powershell-dialer-challenge/
---
+++ TextMe/TextMe.ps1
@@ -0,0 +1,23 @@
+function Convert-TextToDial{
+    param(
+        [ValidateLength(1,5)]
+        [Parameter(Mandatory=$true)]
+        [string]$Word
+    )
+    for($i=0;$i -lt $Word.Length;$i++)
+    {
+        switch -Regex ($Word[$i])
+        {
+            '[a-cA-C]'{$finalResult += '2'}
+            '[d-fD-F]'{$finalResult += '3'}
+            '[g-iG-I]'{$finalResult += '4'}
+            '[j-lJ-L]'{$finalResult += '5'}
+            '[m-oM-O]'{$finalResult += '6'}
+            '[p-sP-S]'{$finalResult += '7'}
+            '[t-vT-V]'{$finalResult += '8'}
+            '[w-zW-Z]'{$finalResult += '9'}
+        }
+    }
+    return $finalResult
+}
+Convert-TextToDial -Word help
